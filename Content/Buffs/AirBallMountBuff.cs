6b28074427ea614580dc672eb4b552a66b4f7a77
Sergei Ivanov <sivanov@networkoptix.com>
2024-06-27 21:51:08 +0200

    AirBall Mount

---
+++ Content/Buffs/AirBallMountBuff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AvatarTheLastAirbender.Content.Buffs
+{
+    public class AirBallMountBuff: ModBuff
+    {
+        public override void SetStaticDefaults()
+		{
+			Main.buffNoTimeDisplay[Type] = true; // The time remaining won't display on this buff
+			Main.buffNoSave[Type] = true; // This buff won't save when you exit the world
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.mount.SetMount(ModContent.MountType<Mounts.AirBall>(), player);
+			player.buffTime[buffIndex] = 10; // reset buff time
+		}
+    }
+}
---
+++ Content/Items/AirBending/AirBallMountItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using AvatarTheLastAirbender.Content.Mounts;
+
+namespace AvatarTheLastAirbender.Content.Items.AirBending
+{
+    public class AirBallMountItem: ModItem
+    {
+        public override void SetDefaults()
+		{
+			Item.width = 20;
+			Item.height = 30;
+			Item.useTime = 20;
+			Item.useAnimation = 20;
+			Item.useStyle = ItemUseStyleID.Swing; // how the player's arm moves when using the item
+			Item.value = Item.sellPrice(gold: 3);
+			Item.rare = ItemRarityID.Green;
+			Item.UseSound = SoundID.Item79; // What sound should play when using the item
+			Item.noMelee = true; // this item doesn't do any melee damage
+			Item.mountType = ModContent.MountType<AirBall>();
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DirtBlock, 1);
+			recipe.Register();
+		}
+    }
+}
