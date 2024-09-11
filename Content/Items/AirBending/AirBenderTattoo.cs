5b68ab3f3c5fdcedaab2446d07df0a554e6ec57c
Sergei Ivanov <sivanov@networkoptix.com>
2024-09-11 22:20:55 +0200

    Fix AirBender Tattoo

--- Content/Items/AirBending/AirBenderTattoo.cs
+++ Content/Items/AirBending/AirBenderTattoo.cs
@@ -8,7 +8,7 @@ namespace AvatarTheLastAirbender.Content.Items.AirBending
 	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
 	// Providing the EquipType.Head value here will result in TML expecting a X_Head.png file to be placed next to the item's main texture.
 	[AutoloadEquip(EquipType.Head)]
-	public class ExampleHelmet : ModItem
+	public class AirBenderTattoo : ModItem
 	{
 		public static readonly int AdditiveGenericDamageBonus = 20;
 
